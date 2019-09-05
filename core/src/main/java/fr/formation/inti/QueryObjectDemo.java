1e78057bc973041a715d1b008f2886bd38efa095
IN-LL-010 <IN-LL-010@IN-LL-010>
2019-09-05 11:09:00 +0200

    hibernate.cfg.xml

---
+++ core/src/main/java/fr/formation/inti/QueryObjectDemo.java
@@ -0,0 +1,40 @@
+package fr.formation.inti;
+
+import java.util.List;
+
+import org.hibernate.query.Query;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+
+import fr.formation.inti.entities.Employee;
+import fr.formation.inti.entities.HibernateUtils;
+
+public class QueryObjectDemo {
+
+	public static void main(String[] args) {
+		SessionFactory factory = HibernateUtils.getSessionFactory();
+		
+		Session session = factory.getCurrentSession();
+		
+		try {
+			session.getTransaction().begin();
+			
+			String sql = "Select e from " + Employee.class.getName() + " e " + " order by e.empName, e.empNo ";
+			
+			Query<Employee> query = session.createQuery(sql);
+			
+	        List<Employee> employees = query.getResultList();
+	           
+	        for (Employee emp : employees) {
+	            System.out.println("Emp: " + emp.getEmpNo() + " : " + emp.getEmpName());
+		}
+		
+	           session.getTransaction().commit();
+	       } catch (Exception e) {
+	           e.printStackTrace();
+
+	           session.getTransaction().rollback();
+	       }	
+				
+	}	
+}
