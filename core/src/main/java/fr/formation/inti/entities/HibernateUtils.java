87a839faf13b6a11d5ea64112890d86bbb87fd9a
IN-LL-010 <IN-LL-010@IN-LL-010>
2019-09-05 13:18:13 +0200

    8

--- core/src/main/java/fr/formation/inti/entities/HibernateUtils.java
+++ core/src/main/java/fr/formation/inti/entities/HibernateUtils.java
@@ -12,12 +12,11 @@ public class HibernateUtils {
  
     private static SessionFactory buildSessionFactory() {
         try {
-            // Create the ServiceRegistry from hibernate.cfg.xml
-            ServiceRegistry serviceRegistry = new StandardServiceRegistryBuilder()//
+            
+            ServiceRegistry serviceRegistry = new StandardServiceRegistryBuilder()								// Créé le ServiceRegistry pour hibernate.cfg.xml
                     .configure("hibernate.cfg.xml").build();
  
-            // Create a metadata sources using the specified service registry.
-            Metadata metadata = new MetadataSources(serviceRegistry).getMetadataBuilder().build();
+            Metadata metadata = new MetadataSources(serviceRegistry).getMetadataBuilder().build();				//création de la metadata
  
             return metadata.getSessionFactoryBuilder().build();
         } catch (Throwable ex) {
@@ -32,7 +31,6 @@ public class HibernateUtils {
     }
  
     public static void shutdown() {
-        // Close caches and connection pools
         getSessionFactory().close();
     }
  
